c3b470066903374896119bdfa7c00972a0d431d2
puxiaoshuai <1372553910@qq.com>
2019-07-18 17:08:20 +0800

    上拉加载下拉刷新

--- lib/pages/home/news.dart
+++ lib/pages/home/news.dart
@@ -1,68 +1,113 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_shop/jsonbean/news_entry.dart';
-class NewsPage extends StatefulWidget  {
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-   String type;
-  NewsPage({Key key,@required this.type});
+class NewsPage extends StatefulWidget {
+
+  String type;
+
+  NewsPage({Key key, @required this.type});
 
   @override
   _NewsPageState createState() => new _NewsPageState();
 }
 
 
-class _NewsPageState extends State<NewsPage> with AutomaticKeepAliveClientMixin   {
-  NewsEntry newsEntry;
-  int curr_page=1;
+class _NewsPageState extends State<NewsPage>
+    with AutomaticKeepAliveClientMixin {
+
+  List<Results> mlistdata = [];
+  int curr_page = 1;
+  RefreshController _refreshController = RefreshController(
+      initialRefresh: true);
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getNews(curr_page);
   }
-  void getNews(int page) async {
-    try{
-      Response response=await Dio().get("http://gank.io/api/data/${widget.type}/10/${page}");
-      setState(() {
-        newsEntry=NewsEntry.fromJson(jsonDecode(response.toString()));
-      });
-    }catch(e){
 
+  Future getNews(int page) async {
+    try {
+      Response response = await Dio().get(
+          "http://gank.io/api/data/${widget.type}/10/${page}");
+      var data = NewsEntry.fromJson(jsonDecode(response.toString()));
+      setState(() {
+        mlistdata.addAll(data.results);
+      });
+      _refreshController.refreshCompleted();
+
+
+    } catch (e) {
+
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (mlistdata.length == 0) {
+      return Center(
+        child: Text("暂无数据"),
+      );
+    } else {
+      return SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        header: MaterialClassicHeader(height: 30.0,),
+
+        onRefresh: (){
+          _pullToRefresh();
+        },
+        onLoading: (){
+          loadMoreData();
+        },
+        controller: _refreshController,
+        child: ListView.builder(itemBuilder: (c, i) {
+           return ListTile(
+             subtitle: new Text(mlistdata[i].desc),
+           );
+        }
+          , itemCount: mlistdata.length,
+          
+         ),
+      );
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    
-    if(newsEntry==null){
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }else{
-      return new RefreshIndicator(child:
-      ListView.builder(itemBuilder: (conent,index){
-        return ListTile(
-          title: Text("今日要闻中"),
-
-          subtitle: new Text(newsEntry.results[index].desc),
-        );
-      },
-        itemCount: newsEntry.results.length,), onRefresh:
-          _pullToRefresh,
-      );
-    }
-
+  void dispose() {
+    // TODO: implement dispose
+    _refreshController.dispose();
+    super.dispose();
   }
-  Future _pullToRefresh() async{
-
-      curr_page=1;
-      newsEntry.results.clear();
-      // getNews(curr_page);
-      return null;
-
+  void _pullToRefresh() async {
+         await Future.delayed(Duration(seconds: 1),(){
+           curr_page = 1;
+           mlistdata.clear();
+           getNews(curr_page);
+           _refreshController.refreshCompleted();
+         });
   }
+
+  loadMoreData() async {
+     await Future.delayed(Duration(seconds: 1),(){
+       this.curr_page++;
+       print("页码"+this.curr_page.toString());
+       getNews(this.curr_page);
+       if(this.curr_page<10){
+         _refreshController.loadComplete();
+       }else{
+         _refreshController.loadNoData();
+       }
+     });
+  }
+
 
   @override
   // TODO: implement wantKeepAlive
